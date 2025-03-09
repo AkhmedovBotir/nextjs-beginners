ae36842c6d5a8fcc62afcb54acb5be56719d20d5
Akhmedov Botir <134036244+AkhmedovBotir@users.noreply.github.com>
2025-03-09 08:00:01 +0500

    next
    navigation router, nav

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
+import { Footer } from "@/components/footer/footer";
+import { Navbar } from "@/components/navbar/navbar";
 import "@/styles/globals.css";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [show , setShow] = useState(true);
+
+  useEffect(() => {
+    if (window.location.pathname.includes('/user')) {
+      setShow(false);
+    }
+  } , [show]);
+  return (
+    <div>
+      {show && <Navbar />}
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,26 +1,13 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 export default function Index() {
-    const user = [
-        {
-            id: 1,
-            name: 'John Doe',
-        },
-        {
-            id: 2,
-            name: 'Jane Doe',
-        },
-        {
-            id: 3,
-            name: 'John Smith',
-        },
-        {
-            id: 4,
-            name: 'Jane Smith',
-        }
-    ]
+    const router = useRouter()
+    const handleRouting = () => {
+        router.push('/about')
+    }
     return (
         <div>
             <Head>
@@ -28,28 +15,18 @@ export default function Index() {
             </Head>
 
             <div className='flex flex-col justify-center items-center h-[100vh] w-[100%]'>
-                {/* <Link href="/about">
+                <Link href="/about">
                     <button>About</button>
                 </Link>
                 <br />
                 <Link href="/contact">
                     <button>Contact</button>
-                </Link> */}
+                </Link>
 
-                <Link href="/user">
+                <button onClick={handleRouting}>About</button>
+                {/* <Link href="/user">
                     <button>User</button>
-                </Link> 
-                <br />
-
-                {
-                    user.map((item, index) => (
-                        <div key={index}>
-                            <Link href={`/user/${item.id}`}>
-                                <h1>{item.name}</h1>
-                            </Link>
-                        </div>
-                    ))
-                }
+                </Link>  */}
             </div>
         </div>
     )
